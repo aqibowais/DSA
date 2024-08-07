9a04a0a662f7f810d52e03edb3b5da80db177909
Code With Aqib <78802058+aqibowais@users.noreply.github.com>
2024-08-08 02:18:58 +0500

    Time: 3 ms (93.32%), Space: 8.5 MB (50.34%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long result = 0;
-        int original = x;
+        long long result = 0;
+        long original = x;
         if(x<0) return false;
-        while(x){
+        while(x != 0){
             result = result*10 + x%10;
             x/=10;
 
         }
-        if(result>INT_MAX || result<INT_MIN) return false;
+
         return result == original;
     }
 };
