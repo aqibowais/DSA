6e93248780b794d484e3f3f701bddc6b9c872434
Code With Aqib <78802058+aqibowais@users.noreply.github.com>
2024-08-15 10:22:37 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
+    int recursive(int x, int res){
+        if(x==0) return res;
+        res = res*10 + x%10;
+        return recursive(x/10, res);
+    }
     bool isPalindrome(int x) {
-        long long result = 0;
-        long original = x;
+        // Recursive approach
         if(x<0) return false;
-        while(x != 0){
-            result = result*10 + x%10;
-            x/=10;
+        int original = x;
+        return recursive(x,0) == original;
 
-        }
+        // long long result = 0;
+        // long original = x;
+        // if(x<0) return false;
+        // while(x != 0){
+        //     result = result*10 + x%10;
+        //     x/=10;
 
-        return result == original;
+        // }
+
+        // return result == original;
     }
 };
