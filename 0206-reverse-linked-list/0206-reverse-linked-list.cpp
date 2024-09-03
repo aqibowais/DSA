e50d10e6c27b6502b80da98fbfd402b5c5077302
Code With Aqib <78802058+aqibowais@users.noreply.github.com>
2024-09-03 09:53:49 +0500

    Time: 8 ms (27.54%), Space: 13 MB (70.25%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,13 +11,25 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        //Recursive TC => 0(N), SC => 0(N)
-        if(head==NULL || head->next == NULL)
-            return head;
-        ListNode* newHead = reverseList(head->next);
-        ListNode* front = head->next;
-        front->next = head;
-        head->next = NULL;
-        return newHead;
+        // //Recursive TC => 0(N), SC => 0(N)
+        // if(head==NULL || head->next == NULL)
+        //     return head;
+        // ListNode* newHead = reverseList(head->next);
+        // ListNode* front = head->next;
+        // front->next = head;
+        // head->next = NULL;
+        // return newHead;
+
+        //Iterative TC => 0(N), SC => 0(1)
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while(temp!=NULL){
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+
     }
 };
