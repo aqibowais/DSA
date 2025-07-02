4983d8d8b74b289e7ce136bca1cc0ef29b13d962
Code With Aqib <78802058+aqibowais@users.noreply.github.com>
2025-07-02 08:47:27 +0500

    Time: 15 ms (42.19%), Space: 17.8 MB (63.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,10 +2,22 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
+        # O(nlogn+mlogm)
         # return sorted(s)==sorted(t)
 
-        countS,countT = {},{}
+        # t(0) = O(n+m) ; s(o) = 1 since we have atmost 26 chars only
+        # countS,countT = {},{}
+        # for i in range(len(s)):
+        #     countS[s[i]] = 1 + countS.get(s[i],0)
+        #     countT[t[i]] = 1 + countT.get(t[i],0)
+        # return countS==countT
+
+
+        # custom approach
+
+        count = [0]*26
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i],0)
-            countT[t[i]] = 1 + countT.get(t[i],0)
-        return countS==countT
+            count[ord(s[i])-ord('a')] += 1
+            count[ord(t[i])-ord('a')] -= 1
+
+        return all(c==0 for c in count)
