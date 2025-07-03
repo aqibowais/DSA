ec88cac321aa9b012bc2af8fe65fc1409b39f6f6
Code With Aqib <78802058+aqibowais@users.noreply.github.com>
2025-07-03 11:57:23 +0500

    Time: 10 ms (27.27%), Space: 24.2 MB (13.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,28 +1,48 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # tc = O(nlogn)
-        count = {}
+        # # tc = O(nlogn)
+        # count = {}
 
-        for i,num in enumerate(nums):
-            count[num] = 1 + count.get(num,0)
+        # for i,num in enumerate(nums):
+        #     count[num] = 1 + count.get(num,0)
 
-        # arr = []
-        # for num,cnt in count.items():
-        #     arr.append([cnt,num])
-        # arr.sort()
+        # # arr = []
+        # # for num,cnt in count.items():
+        # #     arr.append([cnt,num])
+        # # arr.sort()
+        # # res = []
+        # # while len(res)<k:
+        # #     res.append(arr.pop()[1])
+        # # return res
+
+        # # using min-heap tc O(nlogk) n for freq storing and logk for inserting in heap
+        # heap = []
+        # for num in count.keys(): 
+        #     heapq.heappush(heap,(count[num],num)) # count[num] is basically the freq, so storing on the basis of highest freq and no need to store as by default it will be min-heap and store the largest value at bottom
+        #     if len(heap)>k:
+        #         heapq.heappop(heap)
+        
         # res = []
-        # while len(res)<k:
-        #     res.append(arr.pop()[1])
+        # for i in range(k):
+        #     res.append(heapq.heappop(heap)[1])
         # return res
 
-        # using min-heap tc O(nlogk) n for freq storing and logk for deletion in heap
-        heap = []
-        for num in count.keys(): 
-            heapq.heappush(heap,(count[num],num)) # count[num] is basically the freq, so storing on the basis of highest freq and no need to store as by default it will be min-heap and store the largest value at bottom
-            if len(heap)>k:
-                heapq.heappop(heap)
-        
+
+        # Bucket sort tc = 0(n) as n for storing map freq and n for traversing bucket
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num,0)
+
+        bucket = [[] for i in range(len(nums)+1)] # max n+1 size as the max freq will be that
+        for num,freq in count.items():
+            bucket[freq].append(num)
+
         res = []
-        for i in range(k):
-            res.append(heapq.heappop()[1])
-        return res
+        for i in range(len(bucket)-1,0,-1): #from right to left
+            for num in bucket[i]:
+                res.append(num)
+                if (len(res)==k): 
+                    return res
+
+
+       
