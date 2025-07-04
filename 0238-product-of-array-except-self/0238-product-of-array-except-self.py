690eabfe4ed5cefc8987e3fc890a388f47b5c956
Code With Aqib <78802058+aqibowais@users.noreply.github.com>
2025-07-04 12:16:10 +0500

    Time: 16 ms (94.29%), Space: 23.3 MB (79.74%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -8,7 +8,7 @@ class Solution:
         
         post = 1
         for i in range(len(res)-1,-1,-1):
-            res[i] = res[i] * post
+            res[i] *= post
             post*=nums[i]
         
         return res
